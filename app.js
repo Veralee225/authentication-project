3233d2ae6d0c4d1c7af2a47cdafdd648b9cfb6a2
Veralee225 <Veralee225@gmail.com>
2023-05-25 16:07:07 +0100

    feat: login & reg routes

--- app.js
+++ app.js
@@ -7,5 +7,18 @@ app.use(express.json());
 require("dotenv").config();
 require("./config/database").connect();
 
+//Importing user context
+const User = require("./model/user");
+
+//Register
+app.post("/register", (req, res) => {
+    //our register logic goes here
+});
+
+// Login
+app.post("/login", (req, res) => {
+    //our login logic goes here
+})
+
 // const routes = require('./routes');
 module.exports = app;
